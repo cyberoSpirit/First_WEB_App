7dedca2c58e5e65e9bc2cab6c882786727fbcbb1
Julia <julia.hrytsyk@gmail.com>
2017-07-18 22:48:10 +0300

    Movie View, Model  and Controller added.

---
+++ FirstWebApp/FirstWebApp/Controllers/MoviesController.cs
@@ -0,0 +1,22 @@
+﻿using FirstWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstWebApp.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shreck" };
+            //return View(movie);
+            //return Content("Hello world!");
+            //return HttpNotFound();
+            return RedirectToAction("Index", "Home", new { page = 1, sortby = "name" });
+        }
+    }
+}
